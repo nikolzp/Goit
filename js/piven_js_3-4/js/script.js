1e1a8ce3102d1f239d2a227ac868dc7794bcd56c
nikolz <nikolzp@gmail.com>
2017-02-09 18:57:44 +0200

    dz

--- js/piven_js_3-4/js/script.js
+++ js/piven_js_3-4/js/script.js
@@ -1,18 +1,53 @@
-var element = document.createElement('form');
-element.classList.add('form');
-element.innerHTML = 'hello world <br>';
+// var form = document.createElement('form');
+// form.classList.add('form');
+// form.innerHTML = 'question#1 <br>'
+// var body = document.querySelector('body');
+// body.appendChild(form);
+
+
+// var input = document.createElement('input');
+// input.setAttribute('type', 'checkbox');
+// input.style.marginLeft = '10px';
+// var form = document.querySelector('.form');
+// form.appendChild(input);
+
+// var text = document.createElement('span');
+// text.innerHTML = 'olol <br>';
+// form.insertBefore(text, form.input);
+
+
+// var input = document.createElement('input');
+// input.setAttribute('type', 'checkbox');
+// input.style.marginLeft = '10px';
+
+// var text = document.createElement('span');
+// text.innerHTML = 'olol ';
+
+// var form = document.querySelector('.form');
+// form.appendChild(input);
+// form.insertBefore(text, form.input);
+
 
 var body = document.querySelector('body');
-body.appendChild(element);
+body.style.width = '500px';
+body.style.height = '500px';
+body.style.margin = '0 auto';
 
-var liElement = document.createElement('input');
-liElement.type = 'checkbox';
-liElement.innerHTML = '1';
-var ulElement = document.querySelector('.form');
-ulElement.appendChild(liElement);
+var deskr = document.createElement('div');
+deskr.setAttribute('class', 'deskr');
+var h3 = document.createElement('h3');
+h3.innerHTML = 'Тесте по программированию';
+h3.style.textAlign = 'center';
+deskr.appendChild(h3);
+body.appendChild(deskr);
 
-var liElement = document.createElement('input');
-liElement.type = 'checkbox';
-liElement.innerHTML = '2';
-var ulElement = document.querySelector('.form');
-ulElement.appendChild(liElement);
+
+var form = document.createElement('form');
+form.setAttribute('class', 'form');
+body.appendChild(form);
+
+forma = {
+	p = document.createElement('p');
+	input = document.createElement('input');
+	input.setAttribute('type', 'checkbox');
+}
