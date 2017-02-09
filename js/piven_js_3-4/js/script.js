a792cc8fe9584b09a177330ec7d387189aa7c075
nikolz_lin_Home <nikolzp@gmail.com>
2017-02-09 06:24:51 +0200

    dz

--- js/piven_js_3-4/js/script.js
+++ js/piven_js_3-4/js/script.js
@@ -1,16 +1,18 @@
-var element = document.createElement('ul');
-element.classList.add('box');
-element.innerHTML = 'hello world';
+var element = document.createElement('form');
+element.classList.add('form');
+element.innerHTML = 'hello world <br>';
 
 var body = document.querySelector('body');
 body.appendChild(element);
 
-var liElement = document.createElement('li');
+var liElement = document.createElement('input');
+liElement.type = 'checkbox';
 liElement.innerHTML = '1';
-var ulElement = document.querySelector('.box');
+var ulElement = document.querySelector('.form');
 ulElement.appendChild(liElement);
 
-var liElement = document.createElement('li');
+var liElement = document.createElement('input');
+liElement.type = 'checkbox';
 liElement.innerHTML = '2';
-var ulElement = document.querySelector('.box');
+var ulElement = document.querySelector('.form');
 ulElement.appendChild(liElement);
