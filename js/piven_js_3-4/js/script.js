3782419b2d589b7f2701a1415979ebb842484fd7
nikolz <nikolzp@gmail.com>
2017-02-10 17:11:31 +0200

    dz

--- js/piven_js_3-4/js/script.js
+++ js/piven_js_3-4/js/script.js
@@ -1,33 +1,3 @@
-// var form = document.createElement('form');
-// form.classList.add('form');
-// form.innerHTML = 'question#1 <br>'
-// var body = document.querySelector('body');
-// body.appendChild(form);
-
-
-// var input = document.createElement('input');
-// input.setAttribute('type', 'checkbox');
-// input.style.marginLeft = '10px';
-// var form = document.querySelector('.form');
-// form.appendChild(input);
-
-// var text = document.createElement('span');
-// text.innerHTML = 'olol <br>';
-// form.insertBefore(text, form.input);
-
-
-// var input = document.createElement('input');
-// input.setAttribute('type', 'checkbox');
-// input.style.marginLeft = '10px';
-
-// var text = document.createElement('span');
-// text.innerHTML = 'olol ';
-
-// var form = document.querySelector('.form');
-// form.appendChild(input);
-// form.insertBefore(text, form.input);
-
-
 var body = document.querySelector('body');
 body.style.width = '500px';
 body.style.height = '500px';
@@ -36,7 +6,7 @@ body.style.margin = '0 auto';
 var deskr = document.createElement('div');
 deskr.setAttribute('class', 'deskr');
 var h3 = document.createElement('h3');
-h3.innerHTML = 'Тесте по программированию';
+h3.innerHTML = 'Тест по программированию';
 h3.style.textAlign = 'center';
 deskr.appendChild(h3);
 body.appendChild(deskr);
@@ -46,25 +16,38 @@ var form = document.createElement('form');
 form.setAttribute('class', 'form');
 body.appendChild(form);
 
+theme = {
+	tegP: 'p',
+	themeGolos: function(num) {
+		this.tegP = document.createElement('p');
+		this.tegP.innerHTML = num+'.Вопрос №'+num;
+		form.appendChild(this.tegP);
+	}
+}
 
-p = document.createElement('p');
-p.innerHTML = '1. Вопрос №1';
-form.appendChild(p);
+golos = {
+	tegInput: 'input',
+	tegSpan: 'span',
+	func: function(num) {
+		this.tegInput = document.createElement('input');
+		this.tegInput.setAttribute('type', 'checkbox');
+		form.appendChild(this.tegInput);
 
-input = document.createElement('input');
-input.setAttribute('type', 'checkbox');
-form.appendChild(input);
+		this.tegSpan = document.createElement('span');
+		this.tegSpan.innerHTML = 'Вариант ответа №'+num+'<br>';
+		form.appendChild(this.tegSpan);
+	}
+}
 
-span = document.createElement('span');
-span.innerHTML = 'Вариант ответа №1';
-span.style.marginLeft = '10px';
-form.appendChild(span);
-
-input = document.createElement('input');
-input.setAttribute('type', 'checkbox');
-form.appendChild(input);
-
-span = document.createElement('span');
-span.innerHTML = 'Вариант ответа №2';
-span.style.marginLeft = '10px';
-form.appendChild(span);
+theme.themeGolos(1);
+golos.func(1);
+golos.func(2);
+golos.func(3);
+theme.themeGolos(2);
+golos.func(1);
+golos.func(2);
+golos.func(3);
+theme.themeGolos(3);
+golos.func(1);
+golos.func(2);
+golos.func(3);
