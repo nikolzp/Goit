79456926af49e7a69e26bafc5ce2221c977d1535
nikolz <nikolzp@gmail.com>
2017-02-13 18:49:20 +0200

    dz

--- js/piven_js_5-6/js/script.js
+++ js/piven_js_5-6/js/script.js
@@ -1,5 +1,48 @@
-var click = document.querySelector('input');
-click.addEventListener('click', function() {
-	alert('click');
-})
+var body = document.querySelector('body');
 
+var p = document.createElement('p');
+p.innerHTML = '1';
+body.appendChild(p);
+
+// var timer = setInterval(p, 10);
+
+
+console.log((0.1+0.2).toFixed(2));
+
+
+// var click = document.querySelector('input');
+
+// function hendler(event) {
+// 	console.log('x', event.target);
+// }
+
+// // click.addEventListener('click', hendler);
+
+// window.addEventListener('click', hendler)
+// var a = 1;
+
+// setInt = function() {
+// 	console.log(a++)
+// 	if (a === 6) {
+// 		clearInterval(s)
+// 	}
+// }
+
+// var s = setInterval(setInt, 30);
+
+
+// function calc(a, b) {
+// 	console.log('My name is ', this.name, 'i have ', a + b, ' doll' );
+// 	console.log([].slice.call(arguments))
+// }
+
+// var context = {
+// 	name: 'Nikolz',
+// }
+
+// function a(context, a, b) {
+// 	calc.call(context, 3, 4);
+// 	console.log([].slice.call(arguments))
+// }
+
+// a()
