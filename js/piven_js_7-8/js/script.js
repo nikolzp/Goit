a62e14bfd5bb411a55413cf71e399c0586d8fe0d
nikolzp <nikolzp@gmail.com>
2017-03-06 14:38:55 +0200

    dz

--- js/piven_js_7-8/js/script.js
+++ js/piven_js_7-8/js/script.js
@@ -8,5 +8,34 @@ $(function(){
 		$('li').not($submenu).hide();
 	});
 
-})
+	
+	// var $form = $('form');
 
+	
+	$('.firstname').hover(function() {
+		$('.helpsF').addClass('show');
+	},
+	function() {
+		$('.helpsF').removeClass('show');
+	});
+
+	$('.lastname').hover(function () {
+		$('.helpsL').addClass('show');
+	},
+	function() {
+		$('.helpsL').removeClass('show');
+	});
+
+	$('.address').hover(function () {
+		$('.helpsA').addClass('show');
+	},
+	function() {
+		$('.helpsA').removeClass('show');
+	});
+
+	$('.sh-h').click(function () {
+		$('.helpsF').addClass('show');
+		$('.helpsL').addClass('show');
+		$('.helpsA').addClass('show');
+	})
+})
