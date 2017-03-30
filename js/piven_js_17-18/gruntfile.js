a6e2d442f580e71e40614723a5d4d3f695425d5f
nikolzp <nikolzp@gmail.com>
2017-03-30 18:39:44 +0300

    dz

---
+++ js/piven_js_17-18/gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/src/*.js'],
+        dest: 'js/dist/script.main.js'
+      }
+    },
+
+    concat: {
+      options: {
+      },
+      dist: {
+        src: ['css/src/*.css'],
+        dest: 'css/dist/style.main.css'
+      }
+    },
+
+    uglify: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/src/*.js'],
+        dest: 'js/dist/script.min.js'
+      }
+    },
+
+    cssmin: {
+	  target: {
+	    files: {
+	      'css/dist/style.min.css': ['css/src/*.css']
+	    }
+	  }
+	}
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+
+};
